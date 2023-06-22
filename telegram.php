80ad0517e452472752b621ce8a80dbd5115a353b
ntn20b <118632214+ntn20b@users.noreply.github.com>
2023-06-22 17:13:56 +0300

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "6062792211:AAG43jNK_gXVzygXyoX3kbf20khAqPUz4yQ";
+$chat_id = "-953022145";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: local.html');
+} else {
+  echo "Error";
+}
+?>
