3e4b8d2ec5dc6859b33401862f3bbc5511bf4574
ntn20b <118632214+ntn20b@users.noreply.github.com>
2023-06-23 00:38:38 +0300

    Add files via upload

--- sendmessage.php
+++ sendmessage.php
@@ -1,26 +1,21 @@
 <?php
-$output = "Тестовая строка для проверки отображения ошибок PHP";
-
-// Запись вывода в файл
-$file = 'output.txt';
-file_put_contents($file, $output);
 
 $content = '';
 foreach ($_POST as $key => $value) {
-if ($value){
-$content .= "<b>$key</b>: <i>$value</i>\n";
+    if ($value) {
+        $content .= "$key $value\n";
+    }
 }
-}
-if(trim($content)){
-$content = "<b>Message from Site:</b>\n".$content;
-// Your bot's token that got from @BotFather
-$apiToken = '6062792211:AAG43jNK_gXVzygXyoX3kbf20khAqPUz4yQ';
-$data = [
-    'chat_id' => '@designFrankfurtBotG',
-    'text' => $content,
-    'parse-mode' => 'HTML'
-];
-$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?".http_build_query($data));
+if (trim($content)) {
+    $content = "<b>Message from Site:</b>\n".$content;
+    // Your bot's token that got from @BotFather
+    $apiToken = "6062792211:AAG43jNK_gXVzygXyoX3kbf20khAqPUz4yQ";
+    $data = [
+        'chat_id' => '@designFrankfurtBotG',
+        'text' => $content,
+        'parse-mode' => 'HTML'
+    ];
+    $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?".http_build_query($data));
 }
 
 ?>
