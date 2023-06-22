67b731fe812592f010579694bd45dfbce25291d4
ntn20b <118632214+ntn20b@users.noreply.github.com>
2023-06-22 17:44:53 +0300

    Add files via upload

--- telegram.php
+++ telegram.php
@@ -1,28 +1,16 @@
 <?php
+define ('url',"6062792211:AAG43jNK_gXVzygXyoX3kbf20khAqPUz4yQ");
 
-/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
-где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+$name = $_GET['name'];
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email'];
-$token = "6062792211:AAG43jNK_gXVzygXyoX3kbf20khAqPUz4yQ";
-$chat_id = "-953022145";
-$arr = array(
-  'Имя пользователя: ' => $name,
-  'Телефон: ' => $phone,
-  'Email' => $email
-);
+$message = $_GET['message'];
 
-foreach($arr as $key => $value) {
-  $txt .= "<b>".$key."</b> ".$value."%0A";
-};
+$phone = $_GET['phone'];
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+$chat_id = '{-953022145}';
 
-if ($sendToTelegram) {
-  header('Location: local.html');
-} else {
-  echo "Error";
-}
+$message = urlencode("Name:".$name."\nPhone: ".$phone."\nMessage: ".$message);
+
+file_get_contents(url."sendmessage?text=".$message."&chat_id=".$chat_id."&parse_mode=HTML");
+
 ?>
